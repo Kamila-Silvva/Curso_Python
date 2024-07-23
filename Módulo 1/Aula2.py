b11b5d1ed30a08440062a5cc6024dc4464b610b5
Kamila-Silvva <kamilaalvesdemiranda@gmail.com>
2024-07-23 16:55:00 -0300

    Novas aulas

--- Módulo 1/Aula2.py
+++ Módulo 1/Aula2.py
@@ -1,3 +1,13 @@
 '''
-Segunda aula
+Segunda aula - números
 '''
+
+print(11) #int
+print(2.2) #float
+print(type(0)) #e uma class
+# O type e usado para mostrar o tipo do dado que esta dentro dele no caso de texto e uma string
+print(5.2 + 7.8) 
+# ctrl + k + crtl + c = comentar código
+
+print(1 == 1) #Boolean muito usado no if de true e false 
+print(1 == 45)
---
+++ Módulo 1/Aula3.py
@@ -0,0 +1,4 @@
+#converter um tipo para o outro
+print(int('1'), type('1'))
+print(float('1') + 1)
+print(bool(' '))
