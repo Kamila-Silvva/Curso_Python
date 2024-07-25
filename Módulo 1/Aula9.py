dc8c3194d2e09aeb4004b2ace88e566c7a223c2d
Kamila-Silvva <kamilaalvesdemiranda@gmail.com>
2024-07-25 16:45:10 -0300

    If e else no python

---
+++ Módulo 1/Aula9.py
@@ -0,0 +1,23 @@
+#if, elif e else
+#se, se não se, se não
+#o else depedede do if mas e idenpedente do elif
+
+# entrada = input('Você quer "entra" ou "sair"? ')
+
+# if entrada == 'entrar':
+#     print('Você entrou no sistema!')
+
+# elif entrada == 'sair': 
+#     print('Você saiu do sistema!')
+# else: 
+#     print('Comando não identificado')
+    
+
+number_one = int(input('digite o priemiro número: '))
+number_two = int(input('Digite o segundo número: '))
+
+if number_one >= number_two:
+    print(number_one, 'maior que', number_two)
+else:
+    print(number_one, 'menor que', number_two)
+
