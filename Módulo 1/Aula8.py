68b4cc139c06f106e0a02f560266164e5c3cb462
Kamila-Silvva <kamilaalvesdemiranda@gmail.com>
2024-07-25 16:35:13 -0300

    utilizando o input

---
+++ Módulo 1/Aula8.py
@@ -0,0 +1,13 @@
+#Input e utilizado para solicitar dados para o usuário
+
+#função
+
+numero1 = int(input('Digite um número: '))
+numero2 = int(input('Digite outro número: '))
+
+print(f'A soma dos números é: {numero1 + numero2}')
+
+# data_de_nascimento = input('Insira a data de nascimento: ') #dessa forma o input ler somente as strings
+# ano_atual = input('Insira a data atual: ')
+
+# print(f'A sua idade é: {data_de_nascimento - ano_atual}')
