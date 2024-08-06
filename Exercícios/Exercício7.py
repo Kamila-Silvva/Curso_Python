c21da3026d8dcb6f4ca578ca3a90027c16472213
Kamila-Silvva <kamilaalvesdemiranda@gmail.com>
2024-08-06 13:54:00 -0300

    Exercícios de fixação

---
+++ Exercícios/Exercício7.py
@@ -0,0 +1,19 @@
+dias = {
+    1: 'Segunda-feira',
+    2: 'Terça-feira',
+    3: 'Quarta-feira',
+    4: 'Quinta-feira',
+    5: 'Sexta-feira',
+    6: 'Sábado',
+    7: 'Domingo'
+}
+
+numero = int(input('Digite um número de 1 á 7: '))
+
+if numero in dias: #número em dias
+    print(dias[numero])
+else:
+    print('Este dia e inválido!!')
+
+
+
