e235010dc89f0f4e94fe2aad9606fd254b4952ec
Kamila-Silvva <kamilaalvesdemiranda@gmail.com>
2024-08-07 16:30:25 -0300

    Exercícios de fixação

---
+++ Exercícios/Exercício8.py
@@ -0,0 +1,8 @@
+login = input('Digite o seu UserName: ')
+senha = input('Digite a senha de 8 dígitos: ')
+
+loginPermitido = 'Kamila'
+senhaPermitida = 'Ksk2209*'
+
+if login == 'Kamila' and login == loginPermitido:
+    print('Bem vindo!!')
