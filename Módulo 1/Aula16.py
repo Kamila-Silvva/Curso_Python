7394c4fe3847cc6a6a113ec40697f3e447855cb8
Kamila-Silvva <kamilaalvesdemiranda@gmail.com>
2024-08-14 14:41:22 -0300

    Interpolação básica de strigns

---
+++ Módulo 1/Aula16.py
@@ -0,0 +1,13 @@
+"""
+Interpolação básica de strinfs
+s - string 
+d e i - int 
+f - float
+x e X - Hexadecimal
+"""
+
+nome = 'Kamila'
+preco = 1254.123456
+varieval = '%s, o preco é R$%.2f' % (nome, preco)
+print(varieval)
+print('O hexadecimal de %d é %08x' % (1500, 1500))
