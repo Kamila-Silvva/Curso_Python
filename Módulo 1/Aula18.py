1e8da8fb6bc1798a0250af81bb6258ad0b8cedb6
Kamila-Silvva <kamilaalvesdemiranda@gmail.com>
2024-08-14 15:46:49 -0300

    Fatiamento de strings

---
+++ Módulo 1/Aula18.py
@@ -0,0 +1,14 @@
+"""
+Fatiamento de strings
+012345678
+Olá mundo
+-987654321
+Fatiamento [i:f:p] [::]
+obs.: a função len retorna a qtd de caracteres da string 
+"""
+
+variavel = 'Olá mundo'
+#print(len(variavel)) #os dois pontos ':' o que indica para o python o fatiamento
+#print(variavel[4:len(variavel):1])
+print(variavel[-1:-5:-1])
+
